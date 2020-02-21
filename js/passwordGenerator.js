8c43361ba1f9116da3ed2782830d24b8416d6329
AlexandreSchmid <alexandre.schmid@neuf.fr>
2020-02-21 18:28:27 +0100

    close to the end

--- js/passwordGenerator.js
+++ js/passwordGenerator.js
@@ -1,48 +1,39 @@
-var sizeMin = document.getElementById('lengthPswd').getAttribute('min')
-var sizeMax = document.getElementById('lengthPswd').getAttribute('max')
 var char = document.getElementsByName('char')
-var counter = 0
+var count = document.getElementById('lengthPswd').value
 var psw = []
 
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 var alphabet = ['a', 'z', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'q', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'w', 'x', 'c', 'v', 'b', 'n']
-var speChar = ['&', '~', '{', '[', '(', '-', '|', '`', '\\', '_', '^', '@', ')', ']', '=', '}', '+', ',', '?', ';', '.', ':', '!', '§', '%', 'µ', '*', '£', '$']
+var alphabetUpper = alphabet.map(function(x) {
+    return x.toUpperCase()
+})
+var speChar = ['&', '~', '{', '[', '(', '-', '|', '`', "\\", '_', '^', '@', ')', ']', '=', '}', '+', ',', '?', ';', '.', ':', '!', '§', '%', 'µ', '*', '£', '$']
 
 document.querySelector('form').addEventListener('submit', function(e, numbers, alphabet, speChar) {
     e.preventDefault()
-    generate(numbers, alphabet, speChar, counter, psw)
+    for (var i = 0; i < Number(count); i++) {
+        generate(numbers, alphabet, speChar, psw)
+    }
 })
-
-function setRange(sizeMin) {
-    document.getElementById("sizePsw").setAttribute('value', sizeMin + 1)
-    console.log(sizeMin + 1)
-}
-
-setRange(sizeMin)
 
 function generate() {
     if (char[0].checked) {
-        var randSpeChar = Math.random() * Number(speChar.length)
+        var randSpeChar = Math.random() * speChar.length
         psw.push(speChar[Math.round(randSpeChar)])
-        counter++
     }
     if (char[1].checked) {
-        var randAlphabet = Math.random() * Number(alphabet.length)
+        var randAlphabet = Math.random() * alphabet.length
         psw.push(alphabet[Math.round(randAlphabet)])
-        counter++
     }
     if (char[2].checked) {
-        var randAlphabet = Math.random() * Number(alphabet.length)
-        psw.push(alphabet[Math.round(randAlphabet)].toUpperCase())
-        counter++
+        var randAlphabet = Math.random() * alphabetUpper.length
+        var letter = alphabetUpper[Math.round(randAlphabet)]
+        psw.push(letter)
     }
     if (char[3].checked) {
-        var randNum = Math.random() * Number(numbers.length)
+        var randNum = Math.random() * numbers.length
         psw.push(numbers[Math.round(randNum)])
-        counter++
     }
-    console.log(psw)
-    console.log(counter)
 }
 
 generate(numbers, alphabet, speChar)
